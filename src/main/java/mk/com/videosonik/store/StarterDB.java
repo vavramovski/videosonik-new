596ac8d9d7cadc82b0fbbad3000e549f25d4d5b7
Viktor Avramovski <viksa_1998@hotmail.com>
2021-04-12 20:50:19 +0200

    fix: product image varchar + starterdb

--- src/main/java/mk/com/videosonik/store/StarterDB.java
+++ src/main/java/mk/com/videosonik/store/StarterDB.java
@@ -10,7 +10,7 @@ import javax.annotation.PostConstruct;
 import java.util.LinkedList;
 import java.util.List;
 
-//@Component
+@Component
 @AllArgsConstructor
 public class StarterDB {
 
@@ -33,48 +33,40 @@ public class StarterDB {
         productList = new LinkedList<>();
         cartList = new LinkedList<>();
         reviewList = new LinkedList<>();
-        User user = new User("Piki", "emailot", "paswrd", new LinkedList<>());
-        Product product = new Product("123", 200, "img", "descrptioasdo", "keywrds", 5,"LED");
+        User user = new User("testest", "emailot", "paswrd", new LinkedList<>());
+//        Product product = new Product("123", 200, "img", "descrptioasdo", "keywrds", 5,"LED");
 
-        userList.add(user);
-        productList.add(product);
+//        userList.add(user);
+//        productList.add(product);
 
 
-        cartList.add(new Cart(11, user, product, 25));
+//        cartList.add(new Cart(11, user, product, 25));
 //        reviewList.add(new Review(2, user, product, "komentar", 2, ));
 
-        userList = userRepositoryJPA.findAll();
+       /* userList = userRepositoryJPA.findAll();
         productList= productRepositoryJPA.findAll();
         for (User u :userList) {
             for (Product p :productList) {
                 cartService.addProductToCart(p,u,5);
             }
-        }
+        }*/
 
-//        userRepositoryJPA.save(user);
+//        if(!userRepositoryJPA.findById(user.getUsername()).isPresent()){
+//            userRepositoryJPA.save(user);
+//        }
+//            User piki = userRepositoryJPA.findById("test1").get();
+
+            Admin admin = new Admin(user, Privileges.admin);
+            adminRepositoryJPA.save(admin);
+
 //        productRepositoryJPA.save(product);
 //        cartRepositoryJPA.save(cartList.get(0));
 //        reviewRepositoryJPA.save(reviewList.get(0));
 
-//        user.setWishlist(productList);
-//
-//        User u = userRepositoryJPA.findById("Likikalkulus").get();
-//        u.setWishlist(productList);
-//        userRepositoryJPA.save(u);
-//
-//        User user1 = userRepositoryJPA.findById("Likikalkulus").get();
-//        user1.setWishlist(new LinkedList<>());
-//        userRepositoryJPA.save(user1);
-//
-//        User piki = userRepositoryJPA.findById("Piki").get();
-//
-//
-//        Admin admin = new Admin(piki,Privileges.sales);
+
 //        Admin pikiadmin = adminRepositoryJPA.findById("Likson").get();
 //        pikiadmin.setPrivileges(Privileges.sales);
 //        adminRepositoryJPA.save(pikiadmin);
 
-//        adminRepositoryJPA.deleteById("Bace");
-//        adminRepositoryJPA.delete(admin);
     }
 }
--- src/main/java/mk/com/videosonik/store/model/Product.java
+++ src/main/java/mk/com/videosonik/store/model/Product.java
@@ -5,6 +5,7 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
@@ -21,6 +22,7 @@ public class Product {
 
     private int price;
 
+    @Column(columnDefinition="TEXT")
     private String image;
 
     private String description;
